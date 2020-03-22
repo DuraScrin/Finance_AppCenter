53c67f06c4bc7c1c378ff6216f4d5f6b3e1d9e46
DuraScrin <cccp_dura@hotmail.com>
2020-03-22 13:28:37 +0100

    Firs commit

---
+++ Finance.Android/obj/Release/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 3547C94865E1672D */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	653
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	130
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 151497
+	.include	"typemap.mj.inc"
